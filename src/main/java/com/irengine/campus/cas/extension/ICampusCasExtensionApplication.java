c46d57891da021e2e7ed9ce15564e655525c6183
Alex Tang <alex.tang@taoware.com>
2015-03-11 06:22:56 +0800

    cas extension initial version

---
+++ src/main/java/com/irengine/campus/cas/extension/ICampusCasExtensionApplication.java
@@ -0,0 +1,12 @@
+package com.irengine.campus.cas.extension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICampusCasExtensionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ICampusCasExtensionApplication.class, args);
+    }
+}
---
+++ src/test/java/com/irengine/campus/cas/extension/ICampusCasExtensionApplicationTests.java
@@ -0,0 +1,20 @@
+package com.irengine.campus.cas.extension;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.irengine.campus.cas.extension.ICampusCasExtensionApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ICampusCasExtensionApplication.class)
+@WebAppConfiguration
+public class ICampusCasExtensionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
